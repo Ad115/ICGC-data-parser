878dec677b64eb5c6aefae4909cd98ac69b8c4ef
Ad115 <a.garcia230395@gmail.com>
2016-05-24 02:58:33 +0000

    Added get_gene_sequences.pl to get the sequences of a given list of genes.
    The new script needs some depuration, it is the first version.
    Also added the folder tests, where all test files for the scripts will be.

--- TSVfilter_test.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-./TSVfilter.pl --tsv=./test_data/head_genes.tsv --out=TSVfilter_out.tsv
---
+++ tests/get_gene_sequences_test.sh
@@ -0,0 +1,7 @@
+# Test script for get_gene_sequences.pl
+
+# Genes to query, just the first 10 genes from https://dcc.icgc.org/search/g
+GENES="TTN,TTN-AS1,CSMD1,TP53,LRP1B,CSMD3,PCDH15,RYR2,CNTNAP2,PCDHA1"
+
+# Send query to program
+echo $GENES | ../get_gene_sequences.pl 
