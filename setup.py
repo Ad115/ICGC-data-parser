9bd743944234dd8e2434f897684fefae6d863a76
Ad115 <a.garcia230395@gmail.com>
2018-09-03 04:23:59 -0500

    Corrected bug that aborted installation.

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['vcf'],  # Optional
+    install_requires=['pyvcf'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
